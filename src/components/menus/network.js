31297fe2ac62416a034cd3468f50eb51da98053d
= <nicekitae@naver.com>
2024-05-07 16:08:36 +0900

    scan ssid리스트 수정

--- src/components/menus/network.js
+++ src/components/menus/network.js
@@ -181,7 +181,7 @@ const Network = ({}) => {
                         <tbody>
                             {ssidList.map((network, index) => (
                                 <tr key={index}>
-                                    <td className="first">{network.ssid}</td>
+                                    <td className="first">{network}</td>
                                     <td className="second">{network.connected ? "Connected" : "Not Connected"}</td>
                                     <td className="button-cell">
                                         {network.connected ? (
