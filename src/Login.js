f867c36d675bbdf91ef55a1d3b651e39bab46859
= <nicekitae@naver.com>
2024-05-13 18:29:00 +0900

    아이디저장 수정 useEffect추가

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 import './style/new.css';
 import { Link, useNavigate} from "react-router-dom";
 const Login = () => {
