958e4ee3eda197a700a9d1b924258446575d314a
= <nicekitae@naver.com>
2024-11-28 17:09:47 +0900

    devId

--- client/src/components/menus/network.js
+++ client/src/components/menus/network.js
@@ -89,7 +89,7 @@ const Network = ({}) => {
             });
 
             setDevInfo({
-                deviceId: data.dev_info.device_id || "N/A",
+                deviceId: data.dev_info.dev_id || "N/A",
                 IP: data.wifi_info.ip || "N/A",
                 SubPort: data.dev_info.sub_port || "N/A",
                 PushPort: data.dev_info.push_port || "N/A",
