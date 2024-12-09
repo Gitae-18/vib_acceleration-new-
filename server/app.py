e53fb5207a5399661c219041204fe6cca7304ea5
= <nicekitae@naver.com>
2024-12-09 13:33:51 +0900

    port change

--- server/app.py
+++ server/app.py
@@ -299,4 +299,4 @@ def home():
     return render_template('index.html', dev_info = dev_info, wifi = wifi) """
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=5000)
