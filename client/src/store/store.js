b0586ca4da88db3fd37a82e55e3012721d05a74d
= <nicekitae@naver.com>
2024-12-11 11:20:07 +0900

    roll-back

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,19 +1,19 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
+//import thunk from 'redux-thunk';
 const initialState = {
     wifi_info: {
-        ip: "N/A",
-        netmask: "N/A",
-        gateway: "N/A",
-        mac: "N/A",
+        ip: "",
+        netmask: "",
+        gateway: "",
+        mac: "",
         ap_mode: false,
     },
     dev_info: {
-        dev_id: "N/A",
-        ip: "N/A",
-        sub_port: "N/A",
-        push_port: "N/A",
-        req_port: "N/A",
+        dev_id: "",
+        ip: "",
+        sub_port: "",
+        push_port: "",
+        req_port: "",
     },
 };
 // Slice 생성
@@ -35,12 +35,11 @@ const networkSlice = createSlice({
         },
     },
 });
-// 액션과 리듀서 내보내기
+
 export const { setWifiInfo, setDevInfo } = networkSlice.actions;
 export const store = configureStore({
     reducer: {
         network: networkSlice.reducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+    }, 
 });
 export default store;
