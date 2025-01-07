ac56f34631adfe98c50f4bf8502f64c46930b5f7
= <nicekitae@naver.com>
2025-01-07 18:33:34 +0900

    input layout

--- client/src/components/menus/general.js
+++ client/src/components/menus/general.js
@@ -208,7 +208,7 @@ const General = () => {
                                                                 <li>
                                                                     <label htmlFor="ip-address">IP</label>
                                                                 </li>
-                                                                <li style={{marginLeft:'138px'}}>
+                                                                <li style={{marginLeft:'147px'}}>
                                                                     <input
                                                                         type="text"
                                                                         id="ip-address"
@@ -225,7 +225,7 @@ const General = () => {
                                                                 <li>
                                                                     <label htmlFor="subnet-mask">Subnet Mask</label>
                                                                 </li>
-                                                                <li style={{marginLeft:'47px'}}>
+                                                                <li style={{marginLeft:'67px'}}>
                                                                     <input
                                                                         type="text"
                                                                         id="subnet-mask"
@@ -242,7 +242,7 @@ const General = () => {
                                                                 <li>
                                                                     <label htmlFor="gateway">Default Gateway</label>
                                                                 </li>
-                                                                <li style={{marginLeft:'50px'}}>
+                                                                <li style={{marginLeft:'49px'}}>
                                                                     <input
                                                                         type="text"
                                                                         id="gateway"
