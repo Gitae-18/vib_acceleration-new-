6a18231c8f80dd1dd2ef2311f249b0a9695882f1
= <nicekitae@naver.com>
2025-01-07 14:18:41 +0900

    connect btn

--- client/src/components/menus/general.js
+++ client/src/components/menus/general.js
@@ -245,7 +245,7 @@ const General = () => {
                                                             </ul>
                                                             <button
                                                                 className="default_button connect-btn"
-                                                                style={{ margin: "0 auto" }}
+                                                                style={{ marginLeft: "30%" }}
                                                                 onClick={() => handleConnectWiFi(selectedSSID, password)}
                                                             >
                                                                 Connect
