c5f3f36da1a7fd39bd329cb2185d343c05f3d60e
= <nicekitae@naver.com>
2025-01-23 17:24:21 +0900

    wifi 10s

--- server/app.py
+++ server/app.py
@@ -213,7 +213,7 @@ def connect_wifi():
         is_ap = wifi.check_ap_mode()
         if is_ap:
             wifi.stop_ap_mode()
-            time.sleep(5)
+            time.sleep(10)
             print('AP mode stopped, waiting...')
             
         rst = wifi.connect_to_wifi(ssid, password, method)
